a953082a98e0398593d583ce4ed4e9a18d0842fe
duhd <hoangdinhdu@gmail.com>
2019-01-29 15:11:08 +0700

    duhd

--- kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
@@ -1,11 +1,27 @@
 package com.example.client
 
+import com.example.flow.ExampleFlow
 import com.example.state.IOUState
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.contracts.Amount
 import net.corda.core.contracts.StateAndRef
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.NetworkHostAndPort
+import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.loggerFor
 import org.slf4j.Logger
+import kotlin.system.measureTimeMillis
+import net.corda.core.identity.Party
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.utilities.OpaqueBytes
+import net.corda.finance.USD
+import net.corda.finance.contracts.asset.Cash
+import net.corda.finance.flows.CashIssueFlow
+import net.corda.finance.flows.CashPaymentFlow
+import java.util.concurrent.Executors
+
 
 /**
  *  Demonstration of using the CordaRPCClient to connect to a Corda Node and
@@ -26,17 +42,58 @@ private class ExampleClientRPC {
         require(args.size == 1) { "Usage: ExampleClientRPC <node address>" }
         val nodeAddress = NetworkHostAndPort.parse(args[0])
         val client = CordaRPCClient(nodeAddress)
+        val proxy = mutableListOf<CordaRPCOps>()
+        val counterPartyName = CordaX500Name("BankA", "Hanoi", "VN")
 
-        // Can be amended in the com.example.MainKt file.
-        val proxy = client.start("user1", "test").proxy
 
-        // Grab all existing and future IOU states in the vault.
-        val (snapshot, updates) = proxy.vaultTrack(IOUState::class.java)
+        //So luong lenh
+        val txCount = 1000
 
-        // Log the 'placed' IOU states and listen for new ones.
-        snapshot.states.forEach { logState(it) }
-        updates.toBlocking().subscribe { update ->
-            update.produced.forEach { logState(it) }
+        //So luong RPC connections
+        val rpc = 31
+        // Khoi tao ket noi RPC
+
+        for (i in 0 .. rpc) {
+            proxy.add(i, client.start("corda", "not_blockchain").proxy)
+            println("RPC Connected...$i")
         }
+
+        val executor = Executors.newFixedThreadPool(32)
+        val otherParty = proxy[1].wellKnownPartyFromX500Name(counterPartyName)
+
+        //Getbalance
+        val vault = proxy[1].vaultQueryBy<Cash.State>().states
+        var ownedQuantity = vault.fold(0L) { sum, state ->
+            sum + state.state.data.amount.quantity
+        }
+        println("Sum Total: $ownedQuantity")
+
+
+        val forLoopMillisElapsed = measureTimeMillis {
+            var p = 0
+            for (i in 0 .. 1000) {
+                //val notary = proxy.notaryIdentities().first()
+                val worker = Runnable {
+                    if (otherParty != null) {
+                        generateTransactions(proxy[p], otherParty, i)
+                    }
+                }
+                executor.execute(worker)
+                if (p < rpc) p.plus(1) else p = 0
+            }
+            executor.shutdown()
+            while (!executor.isTerminated) {
+        }
+        }
+        println("forLoopMillisElapsed: $forLoopMillisElapsed")
+        println("Finished all threads")
     }
+
+    fun generateTransactions(proxy: CordaRPCOps, otherParty: Party, i: Int) {
+        //println("$i..." + proxy.startFlow(ExampleFlow::Initiator, 99, otherParty).returnValue.getOrThrow().toString())
+        //proxy.startFlow(ExampleFlow::Initiator, 99, otherParty)
+        println("$i..." + proxy.startFlow(::CashPaymentFlow, Amount(1, USD), otherParty).returnValue.getOrThrow().toString())
+    }
+
+
 }

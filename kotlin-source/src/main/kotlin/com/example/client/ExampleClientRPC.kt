0b78e46553344fdbbdcebd4434688db2a22c4b74
duhd <hoangdinhdu@gmail.com>
2019-01-31 17:01:38 +0700

    single thread

--- kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
@@ -45,7 +45,7 @@ private class ExampleClientRPC {
         val client = CordaRPCClient(nodeAddress)
         val conn = client.start("corda", "not_blockchain")
         val proxy = mutableListOf<CordaRPCOps>()
-        val rpcs = 128
+        val rpcs = 256
 
 
         for (i in 0 until rpcs) {
@@ -53,7 +53,7 @@ private class ExampleClientRPC {
             println("RPC Connected...$i")
         }
 
-        val executor = Executors.newFixedThreadPool(128)
+        val executor = Executors.newFixedThreadPool(256)
 
         val counterPartyName = CordaX500Name("BankB", "Hanoi", "VN")
         val otherParty = proxy.first().wellKnownPartyFromX500Name(counterPartyName)

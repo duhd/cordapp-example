6e0790d38efabab5d30428623b5114dc8c9b6ef5
duhd <hoangdinhdu@gmail.com>
2019-01-31 16:45:41 +0700

    single thread

--- kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
@@ -45,7 +45,7 @@ private class ExampleClientRPC {
         val client = CordaRPCClient(nodeAddress)
         val conn = client.start("corda", "not_blockchain")
         val proxy = mutableListOf<CordaRPCOps>()
-        val rpcs = 32
+        val rpcs = 64
 
 
         for (i in 0 until rpcs) {

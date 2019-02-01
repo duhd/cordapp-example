8a96dea39ffd4120251309455c6315ca9ddce433
duhd <hoangdinhdu@gmail.com>
2019-02-01 09:53:24 +0700

    single thread

--- kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
@@ -56,7 +56,7 @@ private class ExampleClientRPC {
             println("RPC Connected...$i")
         }
 
-        val executor: ExecutorService = Executors.newFixedThreadPool(128)
+        val executor: ExecutorService = Executors.newFixedThreadPool(256)
 
         val counterPartyName = CordaX500Name("BankB", "Hanoi", "VN")
         val otherParty = proxy.first().wellKnownPartyFromX500Name(counterPartyName)

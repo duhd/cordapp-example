05d0c919902e3297ab3b6ef82223ab8b093efc09
duhd <hoangdinhdu@gmail.com>
2019-02-13 16:56:04 +0700

    update Jmeter

--- kotlin-source/src/main/kotlin/com/example/client/ClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ClientRPC.kt
@@ -37,7 +37,7 @@ class ClientRPC {
         val counterPartyName = CordaX500Name(receiver, "Hanoi", "VN")
         val otherParty = proxy!!.wellKnownPartyFromX500Name(counterPartyName)
         val random = Random()
-        return cashPayment(proxy!!, otherParty!!, random.nextInt().toLong())
+        return cashPayment(proxy!!, otherParty!!, random.nextInt(9999).toLong())
 
     }
 
@@ -46,13 +46,13 @@ class ClientRPC {
         val counterPartyName = CordaX500Name(receiver, "Hanoi", "VN")
         val otherParty = proxy!!.wellKnownPartyFromX500Name(counterPartyName)
         val random = Random()
-        return cashIssueAndPayment(proxy!!, otherParty!!, notary, random.nextInt().toLong())
+        return cashIssueAndPayment(proxy!!, otherParty!!, notary, random.nextInt(1000).toLong())
     }
 
     fun clientIssue(amount: Long): String {
         val notary = proxy!!.notaryIdentities().first()
         val random = Random()
-        return cashIssue(proxy!!, notary, random.nextInt().toLong())
+        return cashIssue(proxy!!, notary, random.nextInt(10000).toLong())
     }
 
     fun cashPayment(proxy: CordaRPCOps, otherParty: Party, amount: Long): String {
